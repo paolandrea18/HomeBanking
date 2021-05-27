fb3992f24779fc1b02e74a1efb65f64a0360ef70
Paola Cadena <pcadenajoya@hotmail.com>
2021-05-27 07:24:47 -0500

    Work in Progress Register User functionality

---
+++ scripts/user.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(id, name, lastname, email, accountBalance, username, password) {
+    this.id = id;
+    this.name = name;
+    this.lastname = lastname;
+    this.email = email;
+    this.accountBalance = accountBalance;
+    this.username = username;
+    this.password = password;
+  }
+}
